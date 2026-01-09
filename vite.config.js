8bf97c9b1c3e5d50b3a2fb8cfe0dd5e7cb779076
Yk2estrella <maduenoestrellita@gmail.com>
2026-01-08 22:44:52 -0500

    fix vite alias and successful build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: '/fotostudiodigitalmadueno3/',
-  plugins: [react()]
-})
+  base: "/fotostudiodigitalmadueno3/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
