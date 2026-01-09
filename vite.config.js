46a1828b2b180c60510917d8c305b5c3a4414a52
yk2estrella <maduenoestrellita@gmail.com>
2026-01-08 22:12:34 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/fotostudiodigitalmadueno3/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [react()]
 })
